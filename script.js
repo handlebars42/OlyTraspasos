5405756cba97493bd99320e9237baa5deea5c948
Ademir <handlebars42@proton.me>
2026-09-01 17:24:51 -0600

    Lines deleted

--- script.js
+++ script.js
@@ -155,52 +155,6 @@ function onBarcode() {
  })
 
   return
-
-  //TODO: Delete from here
-  navigator.mediaDevices.getUserMedia({
-    audio: 0,
-    video: {
-        facingMode: {
-            ideal: "environment"
-        },
-      zoom: true,
-    }
-  }).then(stream => {
-    barcodeStat.stream = stream;
-
-    const bar = document.getElementById('bar');
-    bar.srcObject = stream;
-    
-    const redline = byId('red-line')
-    redline.style.top = "-" + ((bar.offsetHeight/2)-2) + "px";
-    redline.style.left = (bar.offsetWidth/4) + "px";
-    redline.style.width = ((bar.offsetWidth/4) *2) + "px";
-    
-    const img = byId("input")
-    img.onload = function() {
-      console.log("image loaded")
-      if ('BarcodeDetector' in window) {
-        const detector = new BarcodeDetector({formats:["ean_13", "code_39", "code_128"]});
-        detector.detect(input).then(detections => {
-          if (detections.length == 0) {
-            alert("No se pudo leer el codigo")
-            return
-          }
-
-          detections.forEach(detected => {
-            let result = detected.rawValue||detected.rawData
-            //alert(result);
-
-            bar.srcObject.getTracks().forEach(function(track) {
-              track.stop();
-            }); // Camera stop
-
-            onMostrarArticulo(result)
-          })
-        })
-      }
-    }
-  })
 }
 
 function stopBarcodeCamera() {
@@ -356,31 +310,6 @@ function capturarBarcode() {
   barcodeDetectionTimer = setInterval(detectFrame, 150);
 
   return
-  //TODO: delete from here
-  const bar = byId('bar')
-  const canvas = byId('canvas1');
-  const redline = byId("red-line")
-  const input = byId('input');
-
-  const quarter = bar.videoWidth / 4
-  
-  canvas.width = quarter*2;
-  canvas.height = 200;//bar.videoHeight;
-  const ctx = canvas.getContext('2d');
-  
-  ctx.drawImage(bar, quarter, (bar.videoHeight/2)-100, quarter *2, 200,
-               0, 0, quarter*2, 200);
-  
-  
-    
-  /*canvas.width = bar.videoWidth;
-  canvas.height = bar.videoHeight;
-  const ctx = canvas.getContext('2d');
-  
-  ctx.drawImage(bar, 0, 0);*/
-  
-  input.src = canvas.toDataURL();
-  // Now, the onload event for the img will be fired
 }
 
 function cancelarBarcode() {
